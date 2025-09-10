4efcb6fbb16844f4d50d7eb5763d9f83c81dce20
pawan2505 <pawanaktu2gmail.com>
2025-09-10 09:57:45 +0530

    datatype

--- intro.c
+++ intro.c
@@ -15,13 +15,77 @@ comment
 
 // ()-> prenthesis breket -> parameter list -> arguments -> input to function
 
-#include<stdio.h>
+// #include<stdio.h>
 
-int main(){
+// int main(){
 
-    printf("C Programming samjh aa raha hai!"); // -> statement -> instruction to be executed -> print hello world on the screen
-    return 0;  // 0 -> exit status -> success
+//     printf("C Programming samjh aa raha hai!"); // -> statement -> instruction to be executed -> print hello world on the screen
+//     return 0;  // 0 -> exit status -> success
     
-}
+// }
 
 // printf()// call krna -> invoke krna 
+
+
+// #include<stdio.h>
+
+// int main(){
+
+//     printf("Hello World!!\t");
+//     printf("This is my second C programming class!\t");
+//     printf("Thank You!");
+// }
+
+
+// #include<stdio.h>
+
+// int main(){
+
+//     printf("Hello World!!\n");
+//     printf("This is my second C programming class!\n");
+//     printf("Thank You!");
+// }
+
+
+// #include<stdio.h>
+
+// int main(){
+
+//     printf("Hello World!!");
+//     printf("\nThis is my second C programming class!\n");
+//     printf("Thank You!");
+// }
+
+
+
+// #include<stdio.h>
+
+// int main(){
+
+//     printf("Hello World!!");
+//     printf("\nThis is my second \n C programming class!\n");
+//     printf("Thank You!");
+// }
+
+
+
+// DataType in c
+
+//  1. Integer -> int -> 12,34,56,-12,-45,0 -> 4 bytes
+
+// 2. Float -> float -> 15.36, -9845.36547 -> 4 byte
+
+// 3. double -> 23456.56576869768 , 3455.2324545767 -> 8 bytes
+
+// 4. character -> char -> 'a', 'A' -> 1 bytes
+
+// 5. string -> in c string datatype does not exist -> char array-> char ch[] . *ch -> equal to number of character -> pawan -> 5 character -> 5 bytes
+
+// signed int -> positive and negative both
+// unsigned int -> only positive
+// short int -> 2 bytes
+// long int -> 8 bytes
+// long long int -> 8 bytes
+// long double -> 10 bytes
+// wchar_t -> 2 bytes
+// void -> no value -> return type of function
